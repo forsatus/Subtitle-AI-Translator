b97fd58f14cb94fceff4adb869760d8af2c972bc
Forsatus <88403068+sutasrof@users.noreply.github.com>
2023-11-06 14:46:37 +0100

    Add setup.py script for Subtitle-AI-Translator package

--- setup.py
+++ setup.py
@@ -1,4 +1,45 @@
-﻿from setuptools import setup, find_packages
+﻿"""
+Setup script for the Subtitle-AI-Translator package.
+
+This script uses setuptools to package the Subtitle-AI-Translator project, 
+making it easier to distribute and install. The package is an AI-powered tool 
+that provides seamless subtitle translation to enable cross-language 
+accessibility for video content.
+
+When this script is run, it will package the project according to the 
+configurations specified in the `setup()` function. This includes metadata 
+about the project such as its name, version, description, and more.
+
+The packaged project can then be uploaded to PyPI or installed directly 
+using pip. The script specifies the package's dependencies, which will be 
+automatically installed when the package is installed. It also defines entry 
+points for any command-line scripts included in the package.
+
+Package:
+    Subtitle-AI-Translator
+
+Version:
+    1.0
+
+Author:
+    sutasrof (mario.chauvet@icloud.com)
+
+Repository:
+    https://github.com/sutasrof/Subtitle-AI-Translator
+
+Requirements:
+    Python 3.6 and above
+
+Usage:
+    The `setup.py` script should be run using Python's setuptools module:
+        `python setup.py sdist bdist_wheel`
+    This will generate distribution packages in the `dist` directory.
+
+    The package can then be installed using pip:
+        `pip install .`
+    This will install the package along with its dependencies.
+"""
+from setuptools import setup, find_packages
 
 setup(
     name='subtitle-ai-translator',
